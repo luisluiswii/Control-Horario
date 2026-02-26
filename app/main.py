56644383d34c81cfb1721e0cc21add66b48edbc6
Juanrfjerez99 <juanrfjerez@gmail.com>
2026-02-26 13:12:26 +0100

    Archivos

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-print("Primera prueba")
+from fastapi import FastAPI
+from routers import ejemplo_router
+
+app = FastAPI()
+
+app.include_router(ejemplo_router.router)
+
+@app.get("/")
+def root():
+    return {"message": "API funcionando"}
