4158cec09c6d49b0535038b9c08a11dc1b105afd
Luis <luis@users.noreply.github.com>
2026-03-02 18:17:38 +0100

    fix: initializeDateFormatting for es_ES locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter/gestures.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es_ES', null);
   runApp(const ControlHorarioApp());
 }
 
